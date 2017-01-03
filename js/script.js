de42e586a6b1049afa45fca1b09bc2c895918790
santoscarl <santos_cj28@yahoo.com>
2017-01-02 16:55:56 -0800

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+  $('.home-websites .jumbotron ,.photos-left .jumbotron').css({"background-size" : "115% 115%"});
+
   var color1 = "", color2 = "#fff";
   $(window).scroll(function() {
     if ($(document).scrollTop() > 100) {
