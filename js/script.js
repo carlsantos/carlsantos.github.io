dc66473c50e708aead7812d027311cb2f8f397f2
santoscarl <santos_cj28@yahoo.com>
2017-01-08 00:13:57 -0800

    update

--- js/script.js
+++ js/script.js
@@ -61,9 +61,6 @@ $(document).ready(function(){
       $('.fa-twitter, .fa-facebook').css({"opacity" : "1"});
   });
 
-  $(window).load(function() {
-  		$(".se-pre-con").fadeOut("slow");;
-  	});
 
   /*   ANIMATE   */
   $('.home-right, .home-right h1, .home-right hr, .home-right p, .home-right .btn').css('opacity', 0);
