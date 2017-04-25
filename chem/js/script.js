685c838dfb8d268380b08681b8d9c6eb20aa31fc
santoscarl <cjassantos@gmail.com>
2017-04-25 05:46:06 -0700

    commit

--- chem/js/script.js
+++ chem/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-    if ($(document).scrollTop() > 100) {
+    if ($(document).scrollTop() > 180) {
       $('.navbar-default').addClass('navbar2');
     } else {
       $('.navbar-default').removeClass('navbar2');
