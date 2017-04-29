0f77800d12d839bea035ddfe0969793b9a03bc70
santoscarl <cjassantos@gmail.com>
2017-04-29 07:08:50 -0700

    commit

--- chem/js/script.js
+++ chem/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-    if ($(document).scrollTop() > 180) {
+    if ($(document).scrollTop() > 200) {
       $('.navbar-default').addClass('navbar2');
     } else {
       $('.navbar-default').removeClass('navbar2');
     }
   });
-
+  AOS.init({
+    duration: 1500,
+  })
 });
