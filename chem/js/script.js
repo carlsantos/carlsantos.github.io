adbec2407887eed077559f09ca9b0d19c7e0277d
santoscarl <cjassantos@gmail.com>
2017-04-30 05:57:26 -0700

    lesson

--- chem/js/script.js
+++ chem/js/script.js
@@ -6,7 +6,19 @@ $(document).ready(function(){
       $('.navbar-default').removeClass('navbar2');
     }
   });
+
   AOS.init({
     duration: 1500,
-  })
+  });
+
+  $(function() {
+      $(document).on('click', 'a', function(event) {
+          var $anchor = $(this);
+          $('html, body').stop().animate({
+              scrollTop: $($anchor.attr('href')).offset().top
+          }, 1500, 'easeInOutExpo');
+          event.preventDefault();
+      });
+  });
+
 });
